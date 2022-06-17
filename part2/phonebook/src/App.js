bf5b700ab630032f6f8ab20807627f20a6079d2a
Jaakko Lind <jaakko.lind@tuni.fi>
2022-06-17 11:13:10 +0300

    first draft of phonebook exercise

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newName,
+      id: newName.name,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>debug: {newName}</div>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <input value={newName} onChange={handleNoteChange} />
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
+
+export default App;
